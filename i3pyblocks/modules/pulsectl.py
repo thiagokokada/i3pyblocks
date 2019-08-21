601eaf7ae3692ccd1c226cf37254c6270f79c999
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-08-20 23:35:06 -0300

    Convert PulseAudioModule to use event loop instead of polling

--- i3pyblocks/modules/pulsectl.py
+++ i3pyblocks/modules/pulsectl.py
@@ -1,9 +1,12 @@
+import asyncio
+
 import pulsectl
 
 from i3pyblocks import core, utils
 
-
-class PulseAudioModule(core.PollingModule):
+# Based on:
+# https://github.com/lf-/aiopanel/blob/2786f1116aa2ea0dfe7f331b174104843972fec2/aiopanel.py#L437
+class PulseAudioModule(core.Module):
     def __init__(
         self,
         format: str = "V: {volume:.0f}%",
@@ -30,9 +33,13 @@ class PulseAudioModule(core.PollingModule):
         self.format_mute = format_mute
         self.colors = colors
         self.icons = icons
-        self.pulse = pulsectl.Pulse(__name__)
+        # https://pypi.org/project/pulsectl/#event-handling-code-threads
+        self.pulse = pulsectl.Pulse(__name__, threading_lock=True)
         self._find_default_sink()
         self._update_sink_info()
+
+    def __exit__(self, *args):
+        self.pulse.close()
 
     def _find_default_sink(self) -> None:
         server_info = self.pulse.server_info()
@@ -45,22 +52,53 @@ class PulseAudioModule(core.PollingModule):
     def _update_sink_info(self) -> None:
         self.sink = self.pulse.sink_info(self.sink.index)
 
+    def _handle_event(self, event):
+        if event.facility == "server":
+            self._find_default_sink()
+            self._update_sink_info()
+        elif event.facility == "sink":
+            self._update_sink_info()
+
+    def _event_callback(self, event):
+        self.event = event
+        raise pulsectl.PulseLoopStop()
+
+    def _loop(self) -> None:
+        self.run()
+        self.pulse.event_mask_set("sink", "server")
+        self.pulse.event_callback_set(self._event_callback)
+
+        while True:
+            self.run()
+
+            self.pulse.event_listen()
+
+            self._handle_event(self.event)
+
+    async def loop(self) -> None:
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, self._loop)
+
+    def signal_handler(self, *_, **__):
+        self.run()
+
     def click_handler(self, button: int, **kwargs):
-        if button == utils.Mouse.RIGHT_BUTTON:
+        def toggle_mute():
             if self.sink.mute:
                 self.pulse.mute(self.sink, mute=False)
             else:
                 self.pulse.mute(self.sink, mute=True)
+
+        if button == utils.Mouse.RIGHT_BUTTON:
+            toggle_mute()
         elif button == utils.Mouse.SCROLL_UP:
             self.pulse.volume_change_all_chans(self.sink, 0.05)
         elif button == utils.Mouse.SCROLL_DOWN:
             self.pulse.volume_change_all_chans(self.sink, -0.05)
 
-        super().click_handler(**kwargs)
+        self.run()
 
     def run(self) -> None:
-        self._update_sink_info()
-
         if self.sink.mute:
             self.update(self.format_mute.format(), color=utils.Color.URGENT)
         else:
