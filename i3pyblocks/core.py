c2ec79acb7289290d264755e281bd51214fcd9a8
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-19 23:41:17 -0300

    Fix module when there is no update to show yet

--- i3pyblocks/core.py
+++ i3pyblocks/core.py
@@ -51,7 +51,6 @@ class Runner:
         self.modules[module.id] = module
         # This only works correctly because from Python 3.7+ dict is ordered
         self.results[module.id] = None
-        self.register_task(module.start(self.queue))
 
         if signals:
             self.register_signal(module, signals)
