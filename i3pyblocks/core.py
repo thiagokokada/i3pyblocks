9fb6441c489673a23002d59c390505055012c32c
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-05-02 00:17:17 -0300

    Add initial documentation
    For now, formatted as Markdown based on timothycrosley/portray. The
    actual support for building the documentation will come in a later PR,
    after I found a way to generate the documentation without bringing too
    many dependencies to the project.

--- i3pyblocks/core.py
+++ i3pyblocks/core.py
@@ -22,6 +22,13 @@ async def get_aio_reader(loop: asyncio.AbstractEventLoop) -> asyncio.StreamReade
 
 
 class Runner:
+    """Manages and runs Modules
+
+    This is the class in i3pyblocks responsible to managing Modules and
+    running them, printing its results to stdout and also reading stdin for
+    possible click events comming from i3bar.
+    """
+
     def __init__(self) -> None:
         self.loop = asyncio.get_event_loop()
         self.modules: Dict[uuid.UUID, modules.Module] = {}
@@ -32,6 +39,22 @@ class Runner:
     def register_signal(
         self, module: modules.Module, signums: Iterable[Union[int, signal.Signals]]
     ) -> None:
+        """Registers a list of Unix signals for a Module
+
+        This will register a Module's `signal_handler()` method as a callback
+        for when signums[] is called. Note that since signals are associated
+        with the main thread of i3pyblocks, each signal can only be assigned
+        to an specific Module.
+
+        The received signal will be passed to `Module.signal_handler()` as a
+        parameter, so when receiving multiple signals it is possible to
+        identify each of them separately.
+
+          - *module*: Module's instance that will receive the signal
+          - *signums*: any iterable containing signal numbers, can be either
+        an int or a signal.Signals' enum
+        """
+
         async def signal_handler(sig: signal.Signals):
             try:
                 logger.debug(
@@ -53,6 +76,7 @@ class Runner:
             logger.debug(f"Registered signal {sig.name} for {module.name}")
 
     def register_task(self, awaitable: Awaitable) -> None:
+        """Register a task that will be run in Runner's loop."""
         task = asyncio.create_task(awaitable)
         self.tasks.append(task)
         logger.debug(f"Registered async task {awaitable} in {self}")
@@ -60,6 +84,18 @@ class Runner:
     def register_module(
         self, module: modules.Module, signals: Iterable[Union[int, signal.Signals]] = ()
     ) -> None:
+        """Registers a Module that will be run and managed by Runner
+
+        This will register a new Module in Runner and also make sure that
+        everything is ready to run the Module correctly. Optionally it will
+        also register any Unix signals that the Module wants to wait for
+        events (see `Runner.register_signal()` method).
+
+          - *module*: i3pyblocks.modules.Module's instance that will be
+        registered
+          - *signums*: any iterable containing signal numbers, can be either
+        an int or a signal.Signals' enum
+        """
         self.modules[module.id] = module
         # This only works correctly because from Python 3.7+ dict is ordered
         self.results[module.id] = None
@@ -69,6 +105,7 @@ class Runner:
             self.register_signal(module, signals)
 
     async def update_results(self) -> None:
+        """Get updated results from registed Modules"""
         id_, result = await self.queue.get()
         self.results[id_] = result
 
@@ -82,12 +119,26 @@ class Runner:
                 break
 
     async def write_results(self) -> None:
+        """Writes results to stdout
+
+        This is a endless loop that will wait for an update in any Module,
+        get this update and print all Modules results using the format
+        described in <https://i3wm.org/docs/i3bar-protocol.html#_the_protocol>.
+        """
         while True:
             await self.update_results()
             output = list(self.results.values())
             print(json.dumps(output), end=",\n", flush=True)
 
     async def click_event(self, raw: AnyStr) -> None:
+        """Parses a click event, passing it to a Module's `click_handler()`
+
+        Receives a click event from stdin in JSON format, parsing it, finding
+        the correspondent Module and calling its `click_handler()` method.
+
+          - *raw*: a JSON formatted string with the click event, as described
+        in <https://i3wm.org/docs/i3bar-protocol.html#_click_events>
+        """
         try:
             click_event = json.loads(raw)
             instance = click_event["instance"]
@@ -115,6 +166,7 @@ class Runner:
 
     # Based on: https://git.io/fjbHx
     async def click_events(self) -> None:
+        """Reads stdin for new click events"""
         reader = await get_aio_reader(self.loop)
         await reader.readline()
 
@@ -124,10 +176,23 @@ class Runner:
             await reader.readuntil(b",")
 
     def stop(self) -> None:
+        """Stops the Runner"""
         for task in self.tasks:
             task.cancel()
 
     async def start(self, timeout: Optional[int] = None) -> None:
+        """Starts the Runner
+
+        This will first print the header as specified in
+        <https://i3wm.org/docs/i3bar-protocol.html#_the_protocol>, declaring
+        the capabilities of i3pyblocks to i3bar.
+
+        Afterwards it will run forever printing to stdout the updates from
+        the registered Modules in the format expected by i3bar, and will also
+        reads stdin for any click events coming from i3bar.
+
+          - *timeout*: time in seconds to stop the Runner
+        """
         self.register_task(self.click_events())
         self.register_task(self.write_results())
 
--- i3pyblocks/modules/base.py
+++ i3pyblocks/modules/base.py
@@ -9,6 +9,66 @@ from i3pyblocks import core, utils, types
 
 
 class Module(metaclass=abc.ABCMeta):
+    """Base Module
+
+    This is an abstract class defining the interface for what a Module is.
+    It should not be used directly.
+
+    Each of this method parameters defines the default state of the Module,
+    that is, the value that will be shown unless overriden by Module's
+    update_state() method. For example, let's say you want a module that
+    has a permanent green background (unless overriden), so you can do
+    something like:
+
+    ```python
+    module_instance = Module(background="#008000")
+    ```
+
+    The parameters are based on [i3bar's protocol specification][1],
+    since they're mapped directly (so a `full_text="foo"` results in a
+    `{"full_text": "foo"}`), except for `instance` that is generated
+    randomly.
+
+      - *full*_text: the full_text will be displayed by i3bar on the
+    status line. This is the only required parameter. If full_text is an
+    empty string, the block will be skipped
+      - *short*_text: it will be used in case the status line needs
+    to be shortened because it uses more space than your screen provides
+      - *color*: to make the current state of the information easy to spot,
+    colors can be used. Colors are specified in hex (like in HTML),
+    starting with a leading hash sign. For example, #ff0000 means red
+      - *background*: overrides the background color for this particular
+    block
+      - *border*: overrides the border color for this particular block
+      - *border*_top: defines the width (in pixels) of the top border of
+    this block. Defaults to 1
+      - *border*_right: defines the width (in pixels) of the right border
+    of this block. Defaults to 1
+      - *border*_bottom: defines the width (in pixels) of the bottom
+    border of this block. Defaults to 1
+      - *border*_left: defines the width (in pixels) of the left border of
+    this block. Defaults to 1
+      - *min*_width: The minimum width (in pixels) of the block. If the
+    content of the full_text key take less space than the specified
+    min_width, the block will be padded to the left and/or the right side,
+    according to the align key
+      - *align*: align text on the center, right or left (default) of the
+    block, when the minimum width of the latter, specified by the
+    min_width key, is not reached
+      - *urgent*: a boolean which specifies whether the current value is
+    urgent
+      - *separator*: a boolean which specifies whether a separator line
+    should be drawn after this block. The default is true, meaning the
+    separator line will be drawn
+      - *separator*_block_width: the amount of pixels to leave blank
+    after the block. In the middle of this gap, a separator line will
+    be drawn unless separator is disabled.
+      - *markup*: a string that indicates how the text of the block
+    should be parsed. Pango markup only works if you use a pango font.
+
+    [1]: https://i3wm.org/docs/i3bar-protocol.html#_blocks_in_detail
+    """
+
     def __init__(
         self,
         name: Optional[str] = None,
@@ -70,6 +130,57 @@ class Module(metaclass=abc.ABCMeta):
         separator_block_width: Optional[int] = None,
         markup: Optional[str] = None,
     ) -> None:
+        """Updates Module's state
+
+        The state is what will be shown by the Module in the next update,
+        unless another update occur before a Module's `push_update()` method
+        is called.
+
+        Each of this method arguments is from
+        [i3bar's protocol specification][1], since they're mapped directly
+        (so a `full_text="foo"` results in a `{"full_text": "foo"}`), except
+        for `name` (that is defined once in Module's constructor) and
+        `instance` (that is generate randomly).
+
+          - *full*_text: the full_text will be displayed by i3bar on the
+        status line. This is the only required parameter. If full_text is an
+        empty string, the block will be skipped
+          - *short*_text: it will be used in case the status line needs
+        to be shortened because it uses more space than your screen provides
+          - *color*: to make the current state of the information easy to spot,
+        colors can be used. Colors are specified in hex (like in HTML),
+        starting with a leading hash sign. For example, #ff0000 means red
+          - *background*: overrides the background color for this particular
+        block
+          - *border*: overrides the border color for this particular block
+          - *border*_top: defines the width (in pixels) of the top border of
+        this block. Defaults to 1
+          - *border*_right: defines the width (in pixels) of the right border
+        of this block. Defaults to 1
+          - *border*_bottom: defines the width (in pixels) of the bottom
+        border of this block. Defaults to 1
+          - *border*_left: defines the width (in pixels) of the left border of
+        this block. Defaults to 1
+          - *min*_width: The minimum width (in pixels) of the block. If the
+        content of the full_text key take less space than the specified
+        min_width, the block will be padded to the left and/or the right side,
+        according to the align key
+          - *align*: align text on the center, right or left (default) of the
+        block, when the minimum width of the latter, specified by the
+        min_width key, is not reached
+          - *urgent*: a boolean which specifies whether the current value is
+        urgent
+          - *separator*: a boolean which specifies whether a separator line
+        should be drawn after this block. The default is true, meaning the
+        separator line will be drawn
+          - *separator*_block_width: the amount of pixels to leave blank
+        after the block. In the middle of this gap, a separator line will
+        be drawn unless separator is disabled.
+          - *markup*: a string that indicates how the text of the block
+        should be parsed. Pango markup only works if you use a pango font.
+
+        [1]: https://i3wm.org/docs/i3bar-protocol.html#_blocks_in_detail
+        """
         self._state = utils.non_nullable_dict(
             full_text=full_text,
             short_text=short_text,
@@ -89,9 +200,15 @@ class Module(metaclass=abc.ABCMeta):
         )
 
     def result(self) -> types.Result:
+        """Returns the current state of Module as a Result map
+
+        This will combine both the default state and the current state to
+        generate the current state of the Module.
+        """
         return {**self._default_state, **self._state}
 
     def push_update(self) -> None:
+        """Push result to queue, so it can be retrieved"""
         if not self.freeze:
             self.update_queue.put_nowait((self.id, self.result()))
         else:
@@ -101,10 +218,36 @@ class Module(metaclass=abc.ABCMeta):
             )
 
     def update(self, *args, **kwargs) -> None:
+        """Updates a Module
+
+        This method will immediately updates a Module, so its new contents
+        should be shown in the next redraw of i3bar. It is basically the
+        equivalent of calling Method's `update_state()` followed by
+        `push_update()`.
+
+        The parameters of this method is passed as-is to Module's
+        `update_state()`.
+
+        **See also**: `Module.update_state()` parameters.
+        """
         self.update_state(*args, **kwargs)
         self.push_update()
 
     def abort(self, *args, **kwargs) -> None:
+        """Aborts a Module
+
+        This method will do one last update of the Module, and disable all
+        further updates on it. Useful to show errors.
+
+        Note that this does not necessary stops the Module from working
+        (i.e.: its loop may still be running), it just stops the updates
+        from showing in i3bar.
+
+        The parameters of this method is passed as-is to Module's
+        `update_state()`.
+
+        **See also**: `Module.update_state()` parameters.
+        """
         self.update(*args, **kwargs)
         self.freeze = True
 
@@ -120,13 +263,51 @@ class Module(metaclass=abc.ABCMeta):
         height: int,
         modifiers: List[Optional[str]],
     ) -> None:
+        """Callback called when a click event happens to this Module
+
+        Each of this method arguments is from
+        [i3bar's protocol specification][1], since they're mapped directly
+        (so a `{"x": 1}` results in a `x=1`), except for `name` (that is
+        defined once in Module's constructor) and `instance` (that is
+        generate randomly).
+
+          - *x*: X11 root window coordinates where the click occurred
+          - *y*: X11 root window coordinates where the click occurred
+          - *button*: X11 button ID (for example 1 to 3 for left/middle/right
+        mouse button)
+          - *relative*_x: coordinates where the click occurred, with respect
+        to the top left corner of the block
+          - *relative*_y: coordinates where the click occurred, with respect
+        to the top left corner of the block
+          - *width*: width (in px) of the block
+          - *height*: height (in px) of the block
+          - *modifier*: an array of the modifiers active when the click
+        occurred. The order in which modifiers are listed is not guaranteed.
+
+        [1]: https://i3wm.org/docs/i3bar-protocol.html#_click_events
+        """
         pass
 
     async def signal_handler(self, *, sig: signal.Signals) -> None:
+        """Callback called when a signal event happens to this Module
+
+          - *sig*: signals.Signals' enum with the signal that originated
+        this event
+        """
         pass
 
     @abc.abstractmethod
     async def start(self, queue: asyncio.Queue = None) -> None:
+        """Starts a Module
+
+        This is an abstract method, so it should be overriden by any children.
+        However it is also recommended to call this function from any
+        children, since it does some necessary setup so the Module works
+        correctly.
+
+          - *queue*: asyncio.Queue instance that will be used to notify
+        updates from this Module
+        """
         if not queue:
             queue = asyncio.Queue()
         self.update_queue = queue
@@ -134,12 +315,30 @@ class Module(metaclass=abc.ABCMeta):
 
 
 class PollingModule(Module):
+    """Polling Module
+
+    This is the most simple Module implementation. It is a module that runs
+    a loop where it executes `run()` method and sleeps for `sleep` seconds.
+
+    You must not instantiate this class directly, instead you should
+    subclass it and implement `run()` method first.
+
+      - *sleep*: sleep in seconds between update
+
+    **See also:** `Module()` parameters.
+    """
+
     def __init__(self, sleep: int = 1, **kwargs) -> None:
         super().__init__(**kwargs)
         self.sleep = sleep
 
     @abc.abstractmethod
     async def run(self) -> None:
+        """Main loop in PollingModule
+
+        This is the method that will be run at each X `sleep` seconds. Since
+        this is an abstract method, it should be overriden by its children.
+        """
         pass
 
     async def click_handler(
@@ -171,6 +370,29 @@ class PollingModule(Module):
 
 
 class ExecutorModule(Module):
+    """Executor Module
+
+    This is a special Module implementation to be used when it is not possible
+    to implement functionality using asyncio, for example, when a external
+    library uses its own event loop.
+
+    What this module does is to call `run()` method inside a [executor][1],
+    that is run in a separate thread or process depending of the selected
+    executor. Since it is running in a separate thread/process, it does not
+    interfere with the main asyncio loop.
+
+    You must not instantiate this class directly, instead you should
+    subclass it and implement `run()` method first.
+
+      - *executor*: an optional [`Executor`][2] instance. If not passed it
+    will use the default one
+
+    **See also:** `Module()` parameters.
+
+    [1]: https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.run_in_executor
+    [2]: https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Executor
+    """
+
     def __init__(self, executor: Optional[Executor] = None, **kwargs) -> None:
         super().__init__(**kwargs)
         self.executor = executor
@@ -181,6 +403,11 @@ class ExecutorModule(Module):
 
     @abc.abstractmethod
     def run(self) -> None:
+        """Main loop in PollingModule
+
+        This is the method that will be run inside a executor. Since this
+        is an abstract method, it should be overriden by its children.
+        """
         pass
 
     async def start(self, queue: asyncio.Queue = None) -> None:
