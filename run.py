dea76efae4c0df631d4f43a82c8f6a1a3f8b77a8
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-08-27 20:51:54 -0300

    Temporary workaround for when Backlight is not found

--- run.py
+++ run.py
@@ -86,15 +86,18 @@ async def main():
             ),
         )
     )
-    runner.register_module(
-        aionotify.BacklightModule(
-            format=" {percent:.0f}%",
-            command_on_click=(
-                (types.Mouse.SCROLL_UP, "light -A 5%"),
-                (types.Mouse.SCROLL_DOWN, "light -U 5"),
-            ),
+    try:
+        runner.register_module(
+            aionotify.BacklightModule(
+                format=" {percent:.0f}%",
+                command_on_click=(
+                    (types.Mouse.SCROLL_UP, "light -A 5%"),
+                    (types.Mouse.SCROLL_DOWN, "light -U 5"),
+                ),
+            )
         )
-    )
+    except StopIteration:
+        pass
     runner.register_module(
         pulsectl.PulseAudioModule(format=" {volume:.0f}%", format_mute=" mute")
     )
