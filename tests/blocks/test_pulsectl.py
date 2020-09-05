thiagokokada/i3pyblocks

commit 81b03b01d2cc2c1489095488703a18a6882c6557

Fix deadlock in i3pyblocks.blocks.pulsectl
Instead of using the same instance of Pulse() to control get the updates
and control the volume, use separate ones. This is a workaround, but
until we either rework this module in a proper async or we control the
internal pulsectl event loop correctly, this is as far as we can go.

i3pyblocks/blocks/pulsectl.py
@@ -45,7 +45,7 @@ class PulseAudioBlock(blocks.ExecutorBlock):
         self.subprocess = _subprocess
 
         # https://pypi.org/project/pulsectl/#event-handling-code-threads
-        self.pulse = self.pulsectl.Pulse(__name__, connect=False, threading_lock=True)
+        self.pulse = self.pulsectl.Pulse(__name__, connect=False)
         self._initialize_pulse()
 
     def _initialize_pulse(self):
@@ -110,15 +110,23 @@ class PulseAudioBlock(blocks.ExecutorBlock):
         else:
             self.pulse.mute(self.sink, mute=True)
 
-    async def click_handler(self, button: int, *_, **__) -> None:
+    def change_volume(self, volume: float):
+        # TODO: Use self.pulse instead of our own Pulse instance here
+        # Using self.pulse.volume_change_all_chans() may cause a deadlock
+        # when successive operations are done
+        # We probably need have better control over loop in pulsectl
+        with self.pulsectl.Pulse("volume-changer") as pulse:
+            pulse.volume_change_all_chans(self.sink, volume)
+
+    async def click_handler(self, button: int, **_kwargs) -> None:
         if button == types.MouseButton.LEFT_BUTTON:
             self.subprocess.Popen(self.command)
         elif button == types.MouseButton.RIGHT_BUTTON:
             self.toggle_mute()
         elif button == types.MouseButton.SCROLL_UP:
-            self.pulse.volume_change_all_chans(self.sink, 0.05)
+            self.change_volume(0.05)
         elif button == types.MouseButton.SCROLL_DOWN:
-            self.pulse.volume_change_all_chans(self.sink, -0.05)
+            self.change_volume(-0.05)
 
         self.update_status()
 

tests/blocks/test_pulsectl.py
@@ -1,5 +1,5 @@
 import subprocess
-from unittest.mock import Mock, call
+from unittest.mock import MagicMock, Mock, call
 
 import pulsectl
 import pytest
@@ -18,7 +18,8 @@ ANOTHER_SINK = misc.AttributeDict(
 
 @pytest.fixture
 def pulsectl_mocker():
-    mock_pulsectl = Mock(pulsectl)
+    # TODO: Using Mock(pulsectl) here missed mock to __enter__()
+    mock_pulsectl = MagicMock()
     # Mock Pulse instance class
     mock_pulse = mock_pulsectl.Pulse.return_value
     # Mock pulse.server_list()
@@ -114,7 +115,14 @@ async def test_pulse_audio_block_click_handler(pulsectl_mocker):
     await instance.click_handler(types.MouseButton.RIGHT_BUTTON)
     mute_mock.assert_called_once_with(SINK, mute=True)
 
-    volume_change_mock = mock_pulse.volume_change_all_chans
+    context_manager_mock = mock_pulsectl.Pulse.return_value.__enter__
     await instance.click_handler(types.MouseButton.SCROLL_UP)
     await instance.click_handler(types.MouseButton.SCROLL_DOWN)
-    volume_change_mock.assert_has_calls([call(SINK, 0.05), call(SINK, -0.05)])
+    context_manager_mock.assert_has_calls(
+        [
+            call(),
+            call().volume_change_all_chans(SINK, 0.05),
+            call(),
+            call().volume_change_all_chans(SINK, -0.05),
+        ]
+    )

i3pyblocks/blocks/aionotify.py
@@ -90,7 +90,7 @@ class BacklightBlock(FileWatcherBlock):
 
         self.utils = _utils
 
-    async def click_handler(self, button: int, *_, **__) -> None:
+    async def click_handler(self, button: int, **_kwargs) -> None:
         command = self.command_on_click.get(button)
 
         if not command:

i3pyblocks/blocks/datetime.py
@@ -18,7 +18,7 @@ class DateTimeBlock(blocks.PollingBlock):
         self.format = self.format_time
         self.datetime = _datetime
 
-    async def click_handler(self, *_, **__) -> None:
+    async def click_handler(self, **_kwargs) -> None:
         if self.format == self.format_date:
             self.format = self.format_time
         else:

i3pyblocks/blocks/subprocess.py
@@ -26,7 +26,7 @@ class ShellBlock(blocks.PollingBlock):
         self.color_by_returncode = dict(color_by_returncode)
         self.utils = _utils
 
-    async def click_handler(self, button: int, *_, **__) -> None:
+    async def click_handler(self, button: int, **_kwargs) -> None:
         command = self.command_on_click.get(button)
 
         if not command:
@@ -77,7 +77,7 @@ class ToggleBlock(blocks.PollingBlock):
 
         return bool(output)
 
-    async def click_handler(self, *_, **__) -> None:
+    async def click_handler(self, **_kwargs) -> None:
         state = await self.get_state()
 
         if not state:

commit 5da3d0c9d3bf218407cd00fc6ca7deade6a6d9bd

Fix i3pyblocks.blocks.PulseAudioBlock.toggle_mute()
Probably broken since 81b03b01d2cc2c1489095488703a18a6882c6557.

i3pyblocks/blocks/pulsectl.py
@@ -106,10 +106,11 @@ class PulseAudioBlock(blocks.ExecutorBlock):
             self.update(self.format.format(volume=volume, icon=icon), color=color)
 
     def toggle_mute(self):
-        if self.sink.mute:
-            self.pulse.mute(self.sink, mute=False)
-        else:
-            self.pulse.mute(self.sink, mute=True)
+        with self.pulsectl.Pulse("toggle-mute") as pulse:
+            if self.sink.mute:
+                pulse.mute(self.sink, mute=False)
+            else:
+                pulse.mute(self.sink, mute=True)
 
     def change_volume(self, volume: float):
         # TODO: Use self.pulse instead of our own Pulse instance here

tests/blocks/test_pulsectl.py
@@ -67,9 +67,6 @@ def test_pulse_audio_block(pulsectl_mocker):
 
     assert result["full_text"] == "V: 50%"
 
-    instance.toggle_mute()
-    mock_pulse.mute.assert_called_with(SINK, mute=True)
-
     # If volume is muted, change text and returns Color.URGENT
     mock_pulse.sink_info.return_value = SINK_MUTE
 
@@ -82,9 +79,6 @@ def test_pulse_audio_block(pulsectl_mocker):
 
     assert result["full_text"] == "V: MUTE"
     assert result["color"] == types.Color.URGENT
-
-    instance.toggle_mute()
-    mock_pulse.mute.assert_called_with(SINK_MUTE, mute=False)
 
 
 def test_pulse_audio_block_exception(pulsectl_mocker):
@@ -115,14 +109,26 @@ async def test_pulse_audio_block_click_handler(pulsectl_mocker):
         _subprocess=mock_subprocess,
     )
 
+    # LEFT_BUTTON should run command
     await instance.click_handler(types.MouseButton.LEFT_BUTTON)
     mock_subprocess.Popen.assert_called_once_with("command -c", shell=True)
 
-    mute_mock = mock_pulse.mute
+    context_manager_mock = mock_pulse.__enter__
+    mute_mock = context_manager_mock.return_value.mute
+
+    # When not mute, pressing RIGHT_BUTTON will mute it
+    instance.sink.mute = 0
     await instance.click_handler(types.MouseButton.RIGHT_BUTTON)
     mute_mock.assert_called_once_with(SINK, mute=True)
 
-    context_manager_mock = mock_pulse.__enter__
+    mute_mock.reset_mock()
+
+    # When not mute, pressing RIGHT_BUTTON will unmute it
+    instance.sink.mute = 1
+    await instance.click_handler(types.MouseButton.RIGHT_BUTTON)
+    mute_mock.assert_called_once_with(SINK, mute=False)
+
+    # Scroll Up/Down should trigger volume increase/decrease
     await instance.click_handler(types.MouseButton.SCROLL_UP)
     await instance.click_handler(types.MouseButton.SCROLL_DOWN)
     context_manager_mock.assert_has_calls(